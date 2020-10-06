db1f919d0644ef00a8c345c6b8551822cce02b5a
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2020-10-06 17:03:47 +0200

    movies are displayed from database now and not demo arrray

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,29 +1,20 @@
 const async = require('async');
-
 const Movie = require('../models/movie');
-
-let movies = [
-  {
-    title: "Apocalypse Now",
-    description: "In Vietnam in 1970, Captain Willard (Martin Sheen) takes a perilous and increasingly hallucinatory journey upriver to find and terminate Colonel Kurtz (Marlon Brando), a once-promising officer who has reportedly gone completely mad. In the company of a Navy patrol boat filled with street-smart kids, a surfing-obsessed Air Cavalry officer (Robert Duvall), and a crazed freelance photographer (Dennis Hopper), Willard travels further and further into the heart of darkness.",
-    genres: ["war", 'fiction'],
-    price: 3.50,
-    stock: 50,    
-  },
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    description: "Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths.",
-    genres: ["fantasy", 'fiction'],
-    price: 3.50,
-    stock: 45,    
-  },
-]
 
 let genres = ['Fantasy', 'Science Fiction', 'Fiction', 'Documentary'];
 
 
 exports.index = function(req, res) {
-  res.render('index', { title: 'DankFlix', movies: movies });
+  Movie.find({}, 'title description price stock')
+    .exec(function(err, list_movies) {
+      if(err) {
+        return next(err);
+      }
+      res.render('index', {
+        title: 'DankFlix',
+        movies: list_movies
+      });
+    });
 };
 
 exports.movie_create_get = function(req, res, next) {
@@ -31,14 +22,6 @@ exports.movie_create_get = function(req, res, next) {
 }
 
 exports.movie_create_post = function(req, res) {
-  movies.push({
-    title: req.body.movieTitle,
-    description: req.body.movieDesc,
-    genres: req.body.movieGenre,
-    price: req.body.moviePrice,
-    stock: req.body.movieStock,
-  });
-
   const movie = new Movie({
     title: req.body.movieTitle,
     description: req.body.movieDesc,
@@ -50,31 +33,10 @@ exports.movie_create_post = function(req, res) {
   res.redirect('/');
 };
 
-
-
 /*
-exports.movie_create_post = function(req, res) {
-  movies.push({
-    title: req.body.movieTitle,
-    description: req.body.movieDesc,
-    genres: req.body.movieGenre,
-    price: req.body.moviePrice,
-    stock: req.body.movieStock,
-  });
-
-  const movie = new Movie({
-    title: req.body.movieTitle,
-    description: req.body.movieDesc,
-    price: req.body.moviePrice,
-    stock: req.body.movieStock, 
-  });
-
   movie.save(function(err) {
     if(err){
       console.log(err);
     };
   });
-
-  res.redirect('/');
-}
 */
--- models/movie.js
+++ models/movie.js
@@ -7,7 +7,8 @@ const MovieSchema = new Schema(
     title: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
-    stock: {type: Number, required: true}
+    stock: {type: Number, required: true},
+    //genres: {}
   }
 );
 
